d452641899cbcf59cd576989152aea5e826b3b88
dh <1218029121@qq.com>
2019-11-12 00:14:32 +0800

    添加 spring security

---
+++ register/src/main/java/com/zcx/register/confiure/RegisterWebSecurityConfigure.java
@@ -0,0 +1,19 @@
+package com.zcx.register.confiure;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zcx
+ * spring security 保护注册中心
+ */
+@EnableWebSecurity
+public class RegisterWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}

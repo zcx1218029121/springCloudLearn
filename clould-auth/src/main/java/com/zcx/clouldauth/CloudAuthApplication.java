93377bcb27d637f060461c003bb1f8c446cbeec0
dh <1218029121@qq.com>
2019-11-12 23:59:45 +0800

    auth2权限验证模块验 demo完成

---
+++ clould-auth/src/main/java/com/zcx/clouldauth/CloudAuthApplication.java
@@ -0,0 +1,19 @@
+package com.zcx.clouldauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zcx
+ * eurekaClient
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAuthApplication.class, args);
+    }
+
+}

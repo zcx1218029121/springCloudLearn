41485c2727076c4476584fdae7b6b3fc71c3b8b5
dh <1218029121@qq.com>
2019-11-25 23:28:36 +0800

    用户角色权限设计

--- server/server-test/src/main/java/com/zcx/server/test/ServerTestApplication.java
+++ server/server-test/src/main/java/com/zcx/server/test/ServerTestApplication.java
@@ -1,15 +1,10 @@
 package com.zcx.server.test;
 
 import com.zcx.common.annotation.CloudApplication;
-import com.zcx.common.annotation.EnableServerProtect;
-import com.zcx.common.annotation.EnableZcxAuthExceptionHandler;
-import com.zcx.common.annotation.EnableZcxOAuth2FeignConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**

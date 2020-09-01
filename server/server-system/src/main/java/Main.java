7012fde299f07cbdca9d887b4736ae33faebe8a0
短狐 <1218029121@qq.com>
2020-09-01 18:31:11 +0800

    微服务 docker 部署

---
+++ server/server-system/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @ProjectName: cloud-gateway
+ * @Package: PACKAGE_NAME
+ * @ClassName: Main
+ * @Author: loafer
+ * @Description:
+ * @Date: 2020/8/31 17:39
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = Integer.parseInt("0x4d2",16);
+        System.out.println(a);
+    }
+}
